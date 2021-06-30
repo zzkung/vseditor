5bf0aaf74c3d3e7531b1b96017a53b1cb0f09f68
zzkung <zzkung@outlook.com>
2021-06-30 09:24:37 +0800

    init

---
+++ vseditor-test/bin/index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+const lib = require('vseditor-test-lib')
+console.log(lib)
+console.log('cli test!~!!')
+// 注册一个命令 vseditor-test init 
+// console.log('welcome vseditor-test!!!!!!')
+// const argv = require('process').argv
+
+// console.log(argv)
+
+// 实现参数解析 --version 和 init --name

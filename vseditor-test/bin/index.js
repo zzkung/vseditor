de706c553b8c04110077a786fff8cf64fdbb0ef6
zzkung <zzkung@outlook.com>
2021-06-30 10:32:09 +0800

    update

--- vseditor-test/bin/index.js
+++ vseditor-test/bin/index.js
@@ -1,11 +1,32 @@
 #!/usr/bin/env node
-const lib = require('vseditor-test-lib')
-console.log(lib)
-console.log('cli test!~!!')
-// 注册一个命令 vseditor-test init 
-// console.log('welcome vseditor-test!!!!!!')
-// const argv = require('process').argv
 
-// console.log(argv)
+const lib = require('vseditor-test-lib')
+
+// 注册一个命令 vseditor-test init 
+const argv = require('process').argv
+
+const command = argv[2] // 获取命令
+
+const options = argv.slice(3)
+if (options.length > 1) {
+  let [option, param] = options
+
+  option = option.replace('--', '')
+  if (command) {
+    if (lib[command]) {
+      lib[command]({ option, param })
+    } else {
+      console.log('无效的命令')
+    }
+  } else {
+    console.log('请输入命令')
+  }
+}
 
 // 实现参数解析 --version 和 init --name
+if (command.startsWith('--') || command.startsWith('-')) {
+  const globalOption = command.replace(/--|-/g, '')
+  if (globalOption === 'version' || globalOption === 'V') {
+    console.log('1.0.0')
+  }
+}
